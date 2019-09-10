fd2d284c9d5b1ba111b025930fe488509e7a1b0a
M-Ghani97 <mghani828@gmail.com>
2019-09-09 23:48:20 -0700

    connect app to mongoCluster

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ mongoose.set('useCreateIndex', true);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://M_Ghani97:jx2pn407z@firstcluster-ac9wv.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true 
+    }).then(() => {
+        console.log("DB Connected!");
+    }).catch(err => {
+        console.log(err.message);
+    });
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
 
